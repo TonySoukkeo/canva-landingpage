6025ccb8bdf2ccee06b49a71c5ea1a8828beb7fa
Tony Soukkeo <tonysoukkeodesign@gmail.com>
2020-08-01 13:22:01 -0600

    added content to getting-started.component. And added in tests in getting-started.test

---
+++ src/components/getting-started/getting-started.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GettingStarted = () => {
+  return <div data-test="component-getting-started">Getting started</div>;
+};
+
+export default GettingStarted;
---
+++ src/components/getting-started/getting-started.test.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+// Component
+import GettingStarted from "./getting-started.component";
+
+// Helper functions
+import findByAttr from "../../util/findByAtrr";
+
+/**
+ * initial setup for ShallowWrapper
+ * @function setup
+ * @return {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<GettingStarted />);
+};
+
+test("renders out GettingStarted component without any issues", () => {
+  const wrapper = setup();
+  const component = findByAttr(wrapper, "component-getting-started");
+
+  expect(component.length).toBe(1);
+});
+
+describe("contains necessary CTA buttons", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+  test("has a sign up with Google button", () => {
+    const googleBtn = findByAttr(wrapper, "google-signup-btn");
+
+    expect(googleBtn.length).toBe(1);
+  });
+
+  test("has a sign up with Facebook button", () => {
+    const facebookBtn = findByAttr(wrapper, "facebook-signup-btn");
+
+    expect(facebookBtn.length).toBe(1);
+  });
+
+  test("has a sign up with email button", () => {
+    const emailSignup = findByAttr(wrapper, "email-signup-btn");
+
+    expect(emailSignup.length).toBe(1);
+  });
+
+  test("has a login button", () => {
+    const loginBtn = findByAttr(wrapper, "login-btn");
+
+    expect(loginBtn.length).toBe(1);
+  });
+});
