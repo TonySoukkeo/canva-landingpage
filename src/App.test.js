be7bdd94224f839ceb254db40235de5c1c006ca7
Tony Soukkeo <tonysoukkeodesign@gmail.com>
2020-08-01 14:27:21 -0600

    added testing file for App.js

---
+++ src/App.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+// Component
+import App from "./App";
+import Navigation from "./components/navigation/navigation.component";
+import Card from "./components/card/card.component";
+
+// Helper functions
+import findByAttr from "./util/findByAtrr";
+
+/**
+ * initial setup for ShallowWrapper
+ * @function setup
+ * @return {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
+
+test("renders App component without error", () => {
+  const wrapper = setup();
+  const component = findByAttr(wrapper, "component-app");
+
+  expect(component.length).toBe(1);
+});
+
+describe("App component has correct components inside of it", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  test("contains Navigation component", () => {
+    expect(wrapper.contains(<Navigation />)).toBe(true);
+  });
+
+  test("contains Card component", () => {
+    expect(wrapper.contains(<Card />)).toBe(true);
+  });
+});
