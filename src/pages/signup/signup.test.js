98b34154071bd5934addec4020aa8f46ba4377d4
Tony Soukkeo <tonysoukkeodesign@gmail.com>
2020-08-06 11:20:56 -0600

    tested for button disabled prop on signup.test

--- src/pages/signup/signup.test.js
+++ src/pages/signup/signup.test.js
@@ -98,3 +98,85 @@ describe("test input elements", () => {
     expect(passwordField.props().type).toBe("password");
   });
 });
+
+describe("submit button gets disabled for invalid values", () => {
+  let wrapper;
+  let nameInput;
+  let emailInput;
+  let passwordInput;
+  let nameTargetEvt;
+  let emailTargetEvt;
+  let passwordTargetEvt;
+
+  beforeEach(() => {
+    wrapper = setup();
+
+    nameInput = findByAttr(wrapper, "name-input");
+    emailInput = findByAttr(wrapper, "email-input");
+    passwordInput = findByAttr(wrapper, "password-input");
+
+    nameTargetEvt = {
+      target: {
+        value: "testing",
+        name: "name",
+        focused: true,
+      },
+    };
+
+    emailTargetEvt = {
+      target: {
+        value: "test@test.com",
+        name: "email",
+        focused: true,
+      },
+    };
+
+    passwordTargetEvt = {
+      target: {
+        value: "testing123",
+        name: "password",
+        focused: true,
+      },
+    };
+  });
+
+  test("button is disabled if 'name', 'email', and 'password' field are invalid", () => {
+    const button = findByAttr(wrapper, "signup-button");
+
+    expect(button.prop("disabled")).toBe(true);
+  });
+
+  test("button is disabled if 'name' value is empty", () => {
+    nameInput.simulate("change", nameTargetEvt);
+
+    const signupButton = findByAttr(wrapper, "signup-button");
+
+    expect(signupButton.prop("disabled")).toBe(true);
+  });
+
+  test("button is disabled if 'email' value is invalid", () => {
+    emailInput.simulate("change", emailTargetEvt);
+
+    const signupButton = findByAttr(wrapper, "signup-button");
+
+    expect(signupButton.prop("disabled")).toBe(true);
+  });
+
+  test("button is disabled if 'password' value is invalid", () => {
+    passwordInput.simulate("change", passwordTargetEvt);
+
+    const signupButton = findByAttr(wrapper, "signup-button");
+
+    expect(signupButton.prop("disabled")).toBe(true);
+  });
+
+  test("button is not disabled if all values are valid", () => {
+    nameInput.simulate("change", nameTargetEvt);
+    emailInput.simulate("change", emailTargetEvt);
+    passwordInput.simulate("change", passwordTargetEvt);
+
+    const signupButton = findByAttr(wrapper, "signup-button");
+
+    expect(signupButton.prop("disabled")).toBe(false);
+  });
+});
