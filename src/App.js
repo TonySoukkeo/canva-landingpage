9dbed7e50ccbf18defe056902835fcf1550c1fe8
Tony Soukkeo <tonysoukkeodesign@gmail.com>
2020-08-11 14:55:06 -0600

    refactored components that are rendered via react-router-dom Route by code splitting using React Suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Switch } from "react-router-dom";
 import Route from "./components/route/route.component";
 
@@ -8,21 +8,27 @@ import "./App.css";
 // Styled Components
 import { Main } from "./App.styles";
 
-// Pages
-import GettingStarted from "./pages/getting-started/getting-started.component";
-import LoginContainer from "./pages/login/login-container.component";
-import Signup from "./pages/signup/signup.component";
-import NotFound from "./pages/not-found/not-found.component";
+// Lazy load components
+const GettingStarted = lazy(() =>
+  import("./pages/getting-started/getting-started.component")
+);
+const LoginContainer = lazy(() =>
+  import("./pages/login/login-container.component")
+);
+const Signup = lazy(() => import("./pages/signup/signup.component"));
+const NotFound = lazy(() => import("./pages/not-found/not-found.component"));
 
 function App() {
   return (
     <Main data-test="component-app">
-      <Switch>
-        <Route exact path="/" Component={GettingStarted} navigation />
-        <Route path="/login" Component={LoginContainer} navigation />
-        <Route exact path="/signup" Component={Signup} navigation />
-        <Route Component={NotFound} />
-      </Switch>
+      <Suspense fallback={<div>...loading</div>}>
+        <Switch>
+          <Route exact path="/" Component={GettingStarted} navigation />
+          <Route path="/login" Component={LoginContainer} navigation />
+          <Route exact path="/signup" Component={Signup} navigation />
+          <Route Component={NotFound} />
+        </Switch>
+      </Suspense>
     </Main>
   );
 }
